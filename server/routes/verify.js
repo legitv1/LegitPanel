83e9616996c89a6794be51e20dbe5748ef81b6b2
legitv1 <mohdayan43213.1@gmail.com>
2026-07-12 22:33:38 +0530

    Fix verify API

--- server/routes/verify.js
+++ server/routes/verify.js
@@ -1,80 +1,83 @@
 const express = require("express");
-const key = require("../models/key");
+const Key = require("../models/key");
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
 
-    try{
+    try {
 
-        const { key, deviceId } = req.body;
+        const { key: licenseKey, deviceId } = req.body;
 
-        const license = await key.findOne({ key });
+        const license = await Key.findOne({ key: licenseKey });
 
-        if(!license){
+        if (!license) {
 
             return res.json({
 
-                success:false,
-                message:"Invalid key"
+                success: false,
+                message: "Invalid Key"
 
             });
 
         }
 
-        if(license.used && license.deviceId !== deviceId){
+        if (license.used && license.deviceId !== deviceId) {
 
             return res.json({
 
-                success:false,
-                message:"key already used"
+                success: false,
+                message: "Key already used on another device"
 
             });
 
         }
 
-        if(!license.used){
+        if (!license.used) {
 
             license.used = true;
             license.deviceId = deviceId;
 
             const expire = new Date();
 
-switch(license.duration){
+            switch (license.duration) {
 
-    case "1 Hour":
-        expire.setHours(expire.getHours() + 1);
-        break;
+                case "1 Hour":
+                    expire.setHours(expire.getHours() + 1);
+                    break;
 
-    case "6 Hours":
-        expire.setHours(expire.getHours() + 6);
-        break;
+                case "6 Hours":
+                    expire.setHours(expire.getHours() + 6);
+                    break;
 
-    case "12 Hours":
-        expire.setHours(expire.getHours() + 12);
-        break;
+                case "12 Hours":
+                    expire.setHours(expire.getHours() + 12);
+                    break;
 
-    case "1 Day":
-        expire.setDate(expire.getDate() + 1);
-        break;
+                case "1 Day":
+                    expire.setDate(expire.getDate() + 1);
+                    break;
 
-    case "3 Days":
-        expire.setDate(expire.getDate() + 3);
-        break;
+                case "3 Days":
+                    expire.setDate(expire.getDate() + 3);
+                    break;
 
-    case "7 Days":
-        expire.setDate(expire.getDate() + 7);
-        break;
+                case "7 Days":
+                    expire.setDate(expire.getDate() + 7);
+                    break;
 
-    case "15 Days":
-        expire.setDate(expire.getDate() + 15);
-        break;
+                case "15 Days":
+                    expire.setDate(expire.getDate() + 15);
+                    break;
 
-    case "30 Days":
-        expire.setDate(expire.getDate() + 30);
-        break;
+                case "30 Days":
+                    expire.setDate(expire.getDate() + 30);
+                    break;
 
-}
+                default:
+                    expire.setDate(expire.getDate() + 1);
+
+            }
 
             license.expiresAt = expire;
 
@@ -82,12 +85,12 @@ switch(license.duration){
 
         }
 
-        if(new Date() > license.expiresAt){
+        if (license.expiresAt && new Date() > license.expiresAt) {
 
             return res.json({
 
-                success:false,
-                message:"key Expired"
+                success: false,
+                message: "Key Expired"
 
             });
 
@@ -95,17 +98,19 @@ switch(license.duration){
 
         res.json({
 
-            success:true,
-            expires:license.expiresAt
+            success: true,
+            expires: license.expiresAt
 
         });
 
-    }catch(err){
+    } catch (err) {
+
+        console.error(err);
 
         res.status(500).json({
 
-            success:false,
-            message:err.message
+            success: false,
+            message: err.message
 
         });
 
