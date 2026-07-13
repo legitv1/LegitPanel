df5fbfc13756e13b649577c643c17009ac86604d
legitv1 <mohdayan43213.1@gmail.com>
2026-07-13 08:44:17 +0530

    Fix auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,34 +4,34 @@ const Admin = require("../models/admin");
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
-
-    const { username, password } = req.body;
-
     try {
+        const { username, password } = req.body;
 
-        const admin = await Admin.findOne({ username, password });
+        const admin = await Admin.findOne({
+            username,
+            password
+        });
 
-        if (admin) {
-            return res.json({
-                success: true,
-                role: "Admin"
+        if (!admin) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid username or password"
             });
         }
 
-        return res.status(401).json({
-            success: false,
-            message: "Invalid username or password"
+        return res.json({
+            success: true,
+            role: "Admin"
         });
 
     } catch (err) {
+        console.error(err);
 
         return res.status(500).json({
             success: false,
             message: "Server Error"
         });
-
     }
-
 });
 
 module.exports = router;
